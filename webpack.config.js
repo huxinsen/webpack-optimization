d75773860e8ca768b4214d4ab9957de7500abeea
huxinsen <xinsen.hu@gmail.com>
2019-10-08 11:42:45 +0800

    Add image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,34 @@ module.exports = env => {
         },
         {
           test: /\.(jpe?g|png|gif)$/i,
-          use: 'file-loader',
+          use: [
+            'file-loader',
+            env !== 'development' && {
+              // 使用 file-loader 之前，对图片进行压缩
+              loader: 'image-webpack-loader',
+              // options: {
+              //   mozjpeg: {
+              //     progressive: true,
+              //     quality: 65,
+              //   },
+              //   // optipng.enabled: false will disable optipng
+              //   optipng: {
+              //     enabled: false,
+              //   },
+              //   pngquant: {
+              //     quality: [0.65, 0.9],
+              //     speed: 4,
+              //   },
+              //   gifsicle: {
+              //     interlaced: false,
+              //   },
+              //   // the webp option will enable WEBP
+              //   webp: {
+              //     quality: 75,
+              //   },
+              // },
+            },
+          ].filter(Boolean),
         },
       ],
     },
